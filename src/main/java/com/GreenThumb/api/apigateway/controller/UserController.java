a2fe93fc1daa27982e5e8acc929da5f432b24676
jason engels <j.engels@student.helmo.be>
2025-11-27 11:34:32 +0100

    fix gestion de roles

--- src/main/java/com/GreenThumb/api/apigateway/controller/UserController.java
+++ src/main/java/com/GreenThumb/api/apigateway/controller/UserController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
--- src/main/java/com/GreenThumb/api/config/SecurityConfig.java
+++ src/main/java/com/GreenThumb/api/config/SecurityConfig.java
@@ -6,12 +6,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
